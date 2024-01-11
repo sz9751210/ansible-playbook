c61c3c15c4d79db3ef29f1b57ce467fb0409b10c
alan_wang <sz9751210@gmail.com>
2024-01-11 03:44:15 +0000

    add server playbook

---
+++ gen_server_gce.py
@@ -0,0 +1,57 @@
+from package.generate_conf import setup_configurations, get_inventory_path
+from package.execute_command import run_ansible_playbook
+import sys
+
+if len(sys.argv) != 2:
+    print('didn\'t get playbook path')
+    sys.exit(1)
+
+playbook_path = sys.argv[1]
+default_dir = 'playbook'
+project_id = 'project-id'
+region = 'asia-east1'
+
+group_vars = {
+    'project_id': project_id,
+    'region': region,
+}
+
+inventory_vars = {
+    'group':'server',
+    'hosts': 
+    [
+        {"hostname": "host1", "IP": "10.128.0.21", "zone": "asia-east1-b", "server_type": "nginx"},
+        {"hostname": "host2", "IP": "10.128.0.22", "zone": "asia-east1-b", "server_type": "apache"},
+    ],
+}
+
+instance_vars = {
+    'labels': {
+        'env': 'dev',
+    },
+    'subnet': 'projects/project-id/regions/asia-east1/subnetworks/default',
+    'tags': ['dev', 'test'],
+    'machine_type': 'e2-medium',
+    'boot_disk_size': '20',
+    'boot_disk_type': 'pd-standard',
+    'scopes': 'default'
+}
+
+monitor_vars = {
+    'enabled_node_exporter': 'true',
+}
+
+configurations = {
+    'group_vars'  : (group_vars,      'group_vars/all/env.j2',            'group_vars/all/env.yml'),
+    'inventory'   : (inventory_vars,  'inventory/inventory.instance.j2',  'inventory/inventory.instance.yml'),
+    'instance'    : (instance_vars,   'vars/instance/instance_var.j2',    'vars/instance/vars.yml'),
+    'monitor'     : (monitor_vars,    'vars/monitor/monitor_var.j2',      'vars/monitor/vars.yml')
+}
+
+for key, (content, template_path, output_path) in configurations.items():
+    configurations[key] = (content, f"{default_dir}/{template_path}", f"{default_dir}/{output_path}")
+
+setup_configurations(configurations)
+inventory_path = get_inventory_path(configurations)
+
+run_ansible_playbook(inventory_path, playbook_path)
